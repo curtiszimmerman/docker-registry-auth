85c1812973d35839a6993ce1043d2a5c8a8746c2
curtiszimmerman <curtis.zimmerman@gmail.com>
2015-02-05 00:26:58 -0800

    added: container port specification; fixed: ebkac in sshd_config with port

--- install.sh
+++ install.sh
@@ -6,6 +6,18 @@
 newUserNames=();
 newUserKeys=();
 
+function addUser() {
+    user=$1
+    key=$2
+    echo -n "Adding user ${user}..."
+cat > "tmp/${user}.pub" << EOKEY
+${key}
+EOKEY
+    newUserCommands="RUN useradd -m ${user}\nRUN mkdir -p /home/${user}/.ssh/\nCOPY tmp/${user}.pub /home/${user}/.ssh/${user}.pub\nRUN cat /home/${user}/.ssh/${user}.pub >> /home/${user}/.ssh/authorized_keys\n#%NEWUSERS%"
+    sed -ri "N; s@#%NEWUSERS%@${newUserCommands}@" tmp/Dockerfile.tmp
+    echo "done."
+}
+
 function getUser() {
     read -p "Username: " newUser
     read -p "SSH Public Key: " newKey
@@ -21,24 +33,31 @@ function getUser() {
     newUserKeys=("${newUserKeys[@]}" "${newKey}")
 }
 
-function addUser() {
-    user=$1
-    key=$2
-    echo -n "Adding user ${user} (key ${key})... "
-cat > "tmp/${user}.pub" << EOKEY
-${key}
-EOKEY
-    newUserCommands="RUN useradd -m ${user}\nRUN mkdir -p /home/${user}/.ssh/\nCOPY tmp/${user}.pub /home/${user}/.ssh/${user}.pub\nRUN cat /home/${user}/.ssh/${user}.pub >> /home/${user}/.ssh/authorized_keys\n#%NEWUSERS%"
-    echo "=-=======>(${newUserCommands})"
-    sed -ri "N; s@#%NEWUSERS%@${newUserCommands}@" tmp/Dockerfile.tmp
-    echo "done."
+function setPort() {
+    read -p "What port does the docker-registry container listen on? " regPort
+    if [ ${regPort} -lt 1 ] || [ ${regPort} -gt 65535 ]; then
+        echo "Invalid port! Using default of 5000"
+    fi
+    read -p "What port should the authentication container listen on? " authPort
+    if [ ${authPort} -lt 1 ] || [ ${authPort} -gt 65535 ]; then
+        echo "Invalid port! Using default of 5000"
+    fi
+    newPorts="-p ${regPort}:5000"
+    cp lib/docker-registry.auth.sh.template tmp/docker-registry-auth.tmp
+    sed -ri "s@#%DEFAULTPORT%@${newPorts}@" tmp/docker-registry-auth.tmp
+    cp tmp/docker-registry-auth.sh.tmp docker-registry-auth.sh
+    chmod +x docker-registry-auth.sh
 }
 
 echo -e "\n\n"
 echo "================================================================="
-echo "      Docker-Registry-Auth SSH Auth Layer Add User Utility"
+echo "       Docker-Registry-Auth SSH Auth Layer Install Utility"
 echo -e "================================================================="
 echo "   - To start over, simply run this utility again"
+echo "   - This utility will:"
+echo "     i. Add users and pubkeys to authentication container"
+echo "     ii. Select and set port to listen on"
+echo "     iii. Build the authentication container"
 echo "   - For more information, visit: "
 echo "     https://github.com/curtiszimmerman/docker-registry-auth"
 echo "-----------------------------------------------------------------"
@@ -77,6 +96,13 @@ do
     echo "Adding user ${newUserNames[$i]}..."
     addUser "${newUserNames[${i}]}" "${newUserKeys[${i}]}"
 done
+
+# set AllowUsers in default sshd_config
+cp lib/sshd_config.template tmp/sshd_config
+sed -ri "s@#%ALLOWUSERS%@AllowUsers ${newUserNames[@]}@" tmp/sshd_config
+
+# set ports and create docker-registry-auth.sh
+setPort
 
 echo -n "Creating Dockerfile... "
 cp ./tmp/Dockerfile.tmp ./Dockerfile
